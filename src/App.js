1394a2d6f86b3909d04e9d46efaec3818ffb8975
sma462 <sma462@uclive.ac.nz>
2022-07-04 18:09:22 +1200

    Handling flipped cards

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
         })
         resetTurn()
       }else{
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
     }
   },[choiceOne, choiceTwo])
@@ -75,6 +75,7 @@ function App() {
             key={card.id} 
             card={card} 
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
