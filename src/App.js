a8d4f1e3ef80a7d504d1162299a2b9b18a00dea1
sma462 <sma462@uclive.ac.nz>
2022-07-04 18:33:10 +1200

    Allowing cards to be disabled

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
   // Shuffle the cards (we need two of each card)
   const shuffleCards = () =>{
@@ -39,6 +40,7 @@ function App() {
   // Compare the two selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo){
+      setDisabled(true)
       if (choiceOne.src === choiceTwo.src){
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -63,6 +65,7 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
 
   return (
@@ -76,6 +79,7 @@ function App() {
             card={card} 
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
