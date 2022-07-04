0f7af1ce5700342b527ad2314af65019090b98b0
sma462 <sma462@uclive.ac.nz>
2022-07-04 17:21:51 +1200

    Creating a Card Grid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const cardImages = [
 {"src": "/img/helmet-1.png"},
-{"src": "/img/potion-2.png"},
+{"src": "/img/potion-1.png"},
 {"src": "/img/ring-1.png"},
 {"src": "/img/scroll-1.png"},
 {"src": "/img/shield-1.png"},
@@ -31,6 +31,18 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              {/* Card back is either the "cover" or the actual item */}
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
